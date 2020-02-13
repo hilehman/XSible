c996b4ef35eac48eee5f8b288a1d41e8a18222db
Hillel Lehman <hillel135@gmail.com>
2020-02-13 11:00:04 +0200

    UPDATED

--- app/src/main/java/com/example/myapplication/placeDisplayActivity.java
+++ app/src/main/java/com/example/myapplication/placeDisplayActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
@@ -23,19 +24,19 @@ public class placeDisplayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Toast.makeText(placeDisplayActivity.this, "ברוכים הבאים!", Toast.LENGTH_LONG).show();
-
         super.onCreate(savedInstanceState);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();  //gets in instance of FireStore database
+
         setContentView(R.layout.activity_place_display);
-        PlaceHolder chosenPlace = (PlaceHolder)  getIntent().getSerializableExtra("MyClass");
+        PlaceHolder chosenPlace = (PlaceHolder)  getIntent().getSerializableExtra("MyClass"); //gets PlaceHolder from previous activity
         Map<String, Object>chosenPlaceMap = new HashMap<>();
         chosenPlaceMap.put("id", chosenPlace.getPlaceId());
         chosenPlaceMap.put("name", chosenPlace.getName());
         chosenPlaceMap.put("address", chosenPlace.getAddress());
 
         db.collection("places")
-                .add(chosenPlaceMap)
+                .add(chosenPlaceMap) //
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
@@ -51,30 +52,8 @@ public class placeDisplayActivity extends AppCompatActivity {
         Toast.makeText(this, "SAVED!", Toast.LENGTH_SHORT).show();
         System.out.println("saved");
 
-
-
-
-//        String id = b.getString("ld");
-//        String name = b.getString("name");
-//        String address = b.getString("address");
-//        String grade = PlaceHolder.placesMap.get(id).grade;
-//        TextView textViewName = (TextView) findViewById(R.id.placeName);
-//        TextView textViewAddress = (TextView) findViewById(R.id.placeAddress);
-//        TextView textViewGrade = (TextView) findViewById(R.id.grade);
-//
-//        textViewName.setText(name);
-//        textViewAddress.setText(address);
-//        textViewName.setText(grade);
-//
-//
-//
-//        EditText editTextGrade = (EditText) findViewById(R.id.grade);
-//            String newGrade = editTextGrade.getEditableText().toString();
-//            PlaceHolder.placesMap.get(id).grade = newGrade;
-
-
-
     }
+
 
 
 
