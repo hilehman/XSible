790132f0941cce629bff4826912e1d81cbe9621c
Hillel Lehman <hillel135@gmail.com>
2020-02-27 01:21:38 +0200

    UPDATED

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -59,14 +60,13 @@ public class ResultActivity extends AppCompatActivity implements Serializable {
             Places.initialize(getApplicationContext(), apiKey);
         }
 
-        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.OPENING_HOURS);
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS);
         FetchPlaceRequest request = FetchPlaceRequest.newInstance(chosenPlaceId, placeFields);
         final PlacesClient placesClient = Places.createClient(this);
         placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
-            Place chosenPlace = response.getPlace();
+            Place chosenPlace = response.getPlace(); //gets Place object
             chosenPlaceName = chosenPlace.getName();
             chosenPlaceaddress = chosenPlace.getAddress();
-
             Map<String, Object> chosenPlaceMap = new HashMap<>(); //creates map with place's details
             chosenPlaceMap.put("id", chosenPlace.getId());
             chosenPlaceMap.put("name", chosenPlace.getName());
@@ -74,14 +74,10 @@ public class ResultActivity extends AppCompatActivity implements Serializable {
             chosenPlaceMap.put("link", "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=" + chosenPlace.getId());
             db.collection("places").document(chosenPlaceId) // creates a document named <placeID> and add it to db
                     .set(chosenPlaceMap, SetOptions.merge()); //
-            db.collection("places").document(chosenPlaceId).collection("reviews").document().set(chosenPlaceMap);
             final TextView place_name = (TextView) findViewById(R.id.place_name); //get the id for TextView
             final TextView place_address = (TextView) findViewById(R.id.place_address); //get the id for TextView
-            final TextView place_open_hours = (TextView) findViewById(R.id.place_open_hours); //get the id for TextView
-            place_name.setText(chosenPlaceName); //set the text after clicking button
-            place_address.setText(chosenPlaceaddress); //set the text after clicking button
-            place_open_hours.setText(chosenPlaceOpenHours); //set the text after clicking button
-
+            place_name.setText(chosenPlaceName); //displays Place's name
+            place_address.setText(chosenPlaceaddress); //displays Place's address
         }).addOnFailureListener((exception) -> {
             if (exception instanceof ApiException) {
                 // Handle error with given status code.
@@ -91,7 +87,55 @@ public class ResultActivity extends AppCompatActivity implements Serializable {
 
         setContentView(R.layout.activity_result); //set the layout
         getWindow().getDecorView().setBackgroundColor(0xffa7a9d6);
-        Map<String, String> tempMap = new HashMap<>();
+
+       /* final Button add_review_intent = (Button) findViewById(R.id.add_review_intent);
+        Intent toAddReview = new Intent(this, AddReviewActivity.class);
+        toAddReview.putExtra("chosenPlaceId", chosenPlaceId);
+
+        add_review_intent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(toAddReview);
+            }
+        });*/
+
+
+
+      /*  db.collection("places").document(chosenPlaceId).collection("reviews").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(queryDocumentSnapshots.isEmpty()){
+                            add_review_intent.setVisibility(View.VISIBLE);
+
+                        }
+                        else{
+                            final TextView no_reviews_yet = (TextView) findViewById(R.id.no_reviews_yet);
+                            add_review_intent.setVisibility(View.VISIBLE);
+                            no_reviews_yet.setVisibility(View.GONE);
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ResultActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+
+    }
+
+
+
+
+
+
+/*
+
+
+
+
         final EditText enterOpinion = (EditText) findViewById(R.id.enter_opinion);//get the id for edit text
         Button saveButton;
         saveButton = findViewById(R.id.save_b);//get the id for button
@@ -111,27 +155,9 @@ public class ResultActivity extends AppCompatActivity implements Serializable {
             }
         });
 
+*/
 
-        db.collection("places").document(chosenPlaceId).collection("reviews").get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        if(queryDocumentSnapshots.isEmpty()){
-                            Toast.makeText(ResultActivity.this, "no revire yet", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(ResultActivity.this, "Found review", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(ResultActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
 
-        }
 
 
 
